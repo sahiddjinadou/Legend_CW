84bfa98928b44a890d536c904225e7166aba0237
Moufid Agbannonde <magbannonde@gmail.com>
2024-08-03 13:26:28 +0200

    commentaires et exemples d'utilisation

--- moufid.php
+++ moufid.php
@@ -16,30 +16,50 @@
      * @param mixed $arr
      * @return boolean | string
      */
-    function verify($arr)
+    function verify($arr): bool | string 
     {
+        // vérification du nombre d'éléments du tableau
+        // si ce dernier est différent de 5 éléments
         if(count($arr) !== 5){
-            return "Le tableau n'est pas au complet";
+            // retourne un message 
+            return "Le tableau ne doit contenir qu'exactement cinq éléments";
         } else {
+            // sinon,
+            // comptage du nombre d'occurences du tableau passé en paramètres
         $occurences = array_count_values($arr);
+        // initialisation de deux variables booléennes
+        // triple répétition à false 
      $three_repeat  = false;
+     // et double répétition à false
         $two_repeat = false;
+        // parcours du tableau des occurences avec une boucle foreach
         foreach ($occurences as $value) {
+            // si un élément se répète 3 fois
             if ($value == 3) {
+                // la valeur de la variable de triple répétition passe à true
                 $three_repeat = true;
             }
+            // si un élément se répète 2 fois , la variable de t
             if ($value == 2) {
+                // la variable de double répétition passe à true
                 $two_repeat = true;
             }
         }
+        // si l'on a une triple répétition et une double répétiton 
             if($three_repeat && $two_repeat ){
-                echo "true"  ;
+                // renvoie de true
+                return "true" ;
             } else {
-                echo "false";
+                // sinon renvoi de false
+                return "false";
             }
     }
     };
-    echo (verify(["bo","ca", "ca", "ca","bo"]));
+    // exemple d'utilisation 
+    echo (verify(["bo","ca", "ca", "ca","bo"])); // doit renvoyer true
+    echo (verify(["a", "a", "a", "b", "b"])); // doit renvoyer true
+    echo (verify(["a", "b", "c", "b", "c"])); // doit renvoyer false
+    echo (verify(["a", "a", "a", "a", "a"])); // doit renvoyer false
 
     ?>
 </body>
