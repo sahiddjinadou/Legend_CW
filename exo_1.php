f5da33af09759b2ee8cf6d954c71b92be85d7756
Tino Nafiou <nafiou531@gmail.com>
2024-08-03 17:46:23 +0100

    Correction de l'exo_1 + explication détaillée.

---
+++ exo_1.php
@@ -0,0 +1,34 @@
+<?php
+function verifyArray($array)
+{
+    // Initialiser les variables de compte pour chaque chaîne 
+    $countA = 0;
+    $countB = 0;
+    $countC = 0;
+
+    // Parcourir le tableau et mettre à jour les comptes
+
+    foreach ($array as $element) {
+        if ($element == "a") {
+            $countA++;
+        } elseif ($element == "b") {
+            $countB++;
+        } elseif ($element == "c") {
+            $countC++;
+        }
+    }
+
+    // Je verifie les conditions: une chaîne 3fois, une autre 2 fois
+
+    if (($countA == 3 && $countB == 2)
+        || ($countA == 3 && $countC == 2)
+        || ($countB == 3 && $countA == 2)
+        || ($countB == 3 && $countC == 2)
+        || ($countC == 3 && $countA == 2)
+        || ($countC == 3 && $countB == 2)
+    ) {
+        return true;
+    }
+    return false;
+}
+
