b34437f0868875c7412d997281d52d0f7893a046
Christiane Sossoukpe <christdebo68@gmail.com>
2024-08-06 14:40:43 +0100

    code war

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-function verified($array)
-{
-/*Array _count_value Compte les occurrences de chaque valeur distincte dans un tableau
-source: php.net*/
+function verified($array) {
+    // Compter les occurrences de chaque valeur distincte dans le tableau
     $compteur = array_count_values($array);
-    // Retourne un tableau de valeurs indexé.
-    $nombres = array_values($compteur);
-  
-    // La fonction sort permet de trie mon tableau source: php.net
-    sort($nombres);
-    return $nombres;
+        // Trier les valeurs des comptes pour une comparaison plus facile
+        $nombres = array_values($compteur);
+
+    // Vérifier si le tableau trié des valeurs des comptes est [2, 3]
+    return $nombres == [2, 3];
+
 }
+    // Test cases
+var_dump(verified(["a", "a", "a", "b", "b"])); 
+ 
