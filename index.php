682febf47102467f2ebab8ab76097b45e0f115f0
arnaudsonkpianphp <arnaudsonkpian205@gmail.com>
2024-08-15 11:28:14 +0100

    Code war arrayDiff resolu

--- index.php
+++ index.php
@@ -13,5 +13,24 @@ function verified($array) {
 // Test cases
 var_dump(verified(["a", "a", "a", "b", "b"])); 
 var_dump(verified(["a", "b", "c", "b", "c"])); 
-var_dump(verified(["a", "a", "a", "a", "a"])); 
+var_dump(verified(["a", "a", "a", "a", "a"]));
+
+
+
+//Exercice 2
+
+function arrayDiff($a,$b)
+{
+ return array_diff($a,$b);
+}
+var_dump(arrayDiff([1,2,3,4],[3]));
+
+//la methode  array_diff, prend deux valeurs en paramètre et recherche l'existence 
+// l'existence de la seconde valeur à l'interieur du premier ,tout en suprrimant la seconde valeur 
+//avec son occurrence ;aussi simple que ça !
+
+?>
+
+
+
 
